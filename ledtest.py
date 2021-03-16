71d1075e1bf1d42ec5747b9eb1b12c1308755f87
Fleoid <77473498+Fleoid@users.noreply.github.com>
2021-03-15 18:15:27 -0700

    Update ledtest.py

--- ledtest.py
+++ ledtest.py
@@ -1,45 +1,31 @@
-import Rpi.GPIO as GPIO
-import time
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(4,GPIO.OUT)
-GPIO.setup(17,GPIO.OUT)
-GPIO.setup(22,GPIO.OUT)
+#!/usr/bin/python
+import RPi.GPIO as GPIO   
+GPIO.setmode(GPIO.BCM)  # choose BCM numbering scheme.  
+ 
+   
+GPIO.setup(17, GPIO.OUT)# set GPIO 17 as output for white led  
+GPIO.setup(27, GPIO.OUT)# set GPIO 27 as output for red led  
+GPIO.setup(22, GPIO.OUT)# set GPIO 22 as output for red led
+   
+hz = input('Please define the frequency in Herz(recommended:75): ')
+reddc = input('Please define the red LED Duty Cycle: ')
+greendc = input('Please define the green LED Duty Cycle: ')
+bluedc = input('Please define the blue LED Duty Cycle: ')
+ 
+red = GPIO.PWM(17, hz)    # create object red for PWM on port 17  
+green = GPIO.PWM(27, hz)      # create object green for PWM on port 27   
+blue = GPIO.PWM(22, hz)      # create object blue for PWM on port 22 
+ 
+try:   
+    while True:
+            red.start((reddc/2.55))   #start red led
+        green.start((greendc/2.55)) #start green led
+        blue.start((bluedc/2.55))  #start blue led
+  
+except KeyboardInterrupt:
+        red.stop()   #stop red led
+        green.stop() #stop green led
+        blue.stop()  #stop blue led
+   
+        GPIO.cleanup()          # clean up GPIO on CTRL+C exit  
 
-GPIO.output(4,GPIO.HIGH)
-time.sleep(1)
-GPIO.output(4,GPIO.LOW)
-GPIO.output(17,GPIO.HIGH)
-time.sleep(1)
-GPIO.output(17,GPIO.LOW)
-GPIO.output(22,GPIO.HIGH)
-time.sleep(1)
-GPIO.output(22,GPIO.LOW)
-
-
-
-
-GPIO.output(4,GPIO.HIGH)
-GPIO.output(17,GPIO.HIGH)
-time.sleep(1)
-GPIO.output(17,GPIO.LOW)
-GPIO.output(4,GPIO.LOW)
-
-GPIO.output(17,GPIO.HIGH)
-GPIO.output(22,GPIO.HIGH)
-time.sleep(1)
-GPIO.output(17,GPIO.LOW)
-GPIO.output(22,GPIO.LOW)
-
-GPIO.output(4,GPIO.HIGH)
-GPIO.output(22,GPIO.HIGH)
-time.sleep(1)
-GPIO.output(4,GPIO.LOW)
-GPIO.output(22,GPIO.LOW)
-
-GPIO.output(17,GPIO.HIGH)
-GPIO.output(22,GPIO.HIGH)
-sleep(1)
-GPIO.output(17,GPIO.LOW)
-GPIO.output(22,GPIO.LOW)
-GPIO.cleanup()
-exit()
